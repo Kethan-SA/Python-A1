a86882ff6768f509b3edddb07aae339924bab87a
Kethan S A <74355361+Kethan-SA@users.noreply.github.com>
2021-09-03 19:51:11 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+
+#add 2nos.
+#print(3+5);
+
+
+#addition of int and str
+#a = 5;
+#b='7';
+#print(a+b);
+
+
+
+#Print variable and strings in one line with the help of f string
+#vehicle = "R15v3";
+#speed=150;
+#print(f"The top speed of {vehicle} is {speed}");
+
+
+#to add space and new line in the print statement
+#print("Print variable and strings in one line \t with the help of f string \n How to add space and new line in the print statement")
